6c861122cab5d6f569a59e04c220f7c1daf40079
Ian Lai <laian41@gmail.com>
2013-06-14 14:56:34 -0400

    fixed more stuff in the assignment creation.

--- assignment/static/js/assignment.js
+++ assignment/static/js/assignment.js
@@ -14,42 +14,55 @@ choices = [];
 text = {};
 
 function init(){
+
+	//create sortable
 	$( '#questionsList' ).sortable({
 		update: function(event, ui) {
 	      $('#questionsList>.row-fluid').each(function(index){
-	      	num = index+1
-	      	$(this).id = 'question'+num;
+	      	num = index+1;
+	      	$(this).attr('id', 'question'+num)
 	         $(this).find('.question-edit').attr('onclick', 'load_question('+num+')');
 	         $(this).find('.question-remove').attr('onclick', 'remove_question('+num+')');
 	      });
 	   },
 	});
+
+	//init the dialog
 	$( '#dialog' ).dialog({ 
 		width: document.body.clientWidth*0.50,
-		// maxWidth: document.body.clientWidth*0.8,
 		height: document.body.clientHeight*0.7,
-		// maxHeight: document.body.clientHeight*0.7,
 		modal: true,
 		autoOpen: false,
 		open: function(event, ui){
+			//codemirrors are always reconstructed so they animate correctly
 			code = CodeMirror($('#codediv').get(0), CodeMirrorSettings);
 			solution = CodeMirror($('#solndiv').get(0), CodeMirrorSettings);
 			load_question_helper();
 		},
 		focus: function(event, ui){
+			//disable parent scrolling
 			$('body').addClass('dialog-open');
 		},
 		close: function(event, ui){
+			//re-enable parent scrolling
 			$('body').removeClass('dialog-open');
 		},
 		beforeClose: function( event, ui ) {
+			// warning = 'Are you sure you want to cancel editing?  Any changes will not be saved.';
+			// if(confirm(warning))
+			// 	return true;
+			// else
+			// 	return false;
 		},
+		dialogClass: 'no-close',
 	});
+	//init wysiwyg
 	tinymce.init({
 	   selector: 'textarea#text',
 	   force_p_newlines : false 
 	});
 
+	//initalize datepickers
 	$('#assigndate').datepicker();
 	$('#duedate').datepicker();
 
@@ -62,8 +75,8 @@ function add_choice_div(){
 }
 
 function load_question(num){ 
-	//change save destination
 	$('#questionNum').val(num);
+	//change save destination
 	$('#question-save-button').attr('onclick', 'save_question('+num+')');
 	$( '#dialog' ).dialog('open');
 	//open the dialog
@@ -114,7 +127,7 @@ function save_question(num){
 				<div class="span5 question-description">'+
 				$('#questiontitle').val()+
 				'</div> \
-				<div class="span1 btn" onclick="load_question('+num+')"> \
+				<div class="span1 btn question-edit" onclick="load_question('+num+')"> \
 					<i class="icon-edit-sign"> Edit</i> \
 				</div> \
 				<div class="span1 question-pts"> \
@@ -126,7 +139,6 @@ function save_question(num){
 			</div>';
 		$('#questionsList').append(questionHTML);
 	}
-
 	//create the question object
 	datafield = $('#question'+num).find('input[type=hidden]');
 	question = {
@@ -154,7 +166,8 @@ function save_question(num){
 }
 
 function remove_question(num){
-	$('#question'+num).remove()
+	if(confirm('Are you sure you want to delete this question?'))
+		$('#question'+num).remove()
 }
 
 function remove_question_exists(userid, qid){
@@ -175,8 +188,6 @@ function save(){
 	assignment.title = $('#assigntitle').val();
 	assignment.start = $('#assigndate').datepicker('getDate');
 	assignment.due = $('#duedate').datepicker('getDate');
-
-
 
 	$('#questionsList>.row-fluid').each(function(index){
 		questiondata = $(this).find('input[type=hidden]').val();
