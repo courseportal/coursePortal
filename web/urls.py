9cccc7a88124e0f258773e0299b395893333a473
Nick Terrell <terrelln@umich.edu>
2013-06-25 14:53:19 -0400

    Fixed some minor bugs

--- web/urls.py
+++ web/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^login/?$', 'web.views.account.login', name='login'),
-    url(r'^logout/', 'web.views.account.logout', name='logout'),
+    url(r'^logout/?', 'web.views.account.logout', name='logout'),
     url(r'^register/?$', 'web.views.account.register', name='register'),
-    url(r'^validate/$', 'web.views.account.validate', name='validate'),
+    url(r'^validate/?$', 'web.views.account.validate', name='validate'),
     url(r'^forgot_password/?$', 'web.views.account.forgot_password', name='forgot_password'),
     url(r'^account/?$', 'web.views.account.index', name='account'),
 
@@ -24,7 +24,7 @@ urlpatterns = patterns('',
 
     url(r'^post/(?P<sid>\d+)/?$', 'web.views.home.post', name='post'),
 
-	url(r'^edit-class/(?P<class_id>\d+)/?$', 'web.views.home.edit_class', name='edit_class'),
+	url(r'^edit-class/(?P<class_id>\d+)?/?$', 'web.views.home.edit_class', name='edit_class'),
 	
     url(r'^class/(?P<class_id>\d+)/category/(?P<cat_id>\d+)/atom/(?P<atom_id>\d+)/?$', 'web.views.home.atom', name='atom'),
     url(r'^class/(?P<class_id>\d+)/category/(?P<cat_id>\d+)/?$', 'web.views.home.category', name = 'category'),
@@ -32,7 +32,7 @@ urlpatterns = patterns('',
     url(r'^class-index/?$', 'web.views.home.class_index', name='class_index'),
     url(r'^category/(?P<cat_id>\d+)/?$', 'web.views.home.base_category', name='base_category'),
     url(r'^category/(?P<cat_id>\d+)/atom/(?P<atom_id>\d+)/?$', 'web.views.home.base_atom', name='base_atom'),
-    url(r'/?$', 'web.views.home.index', name='home'),
+    url(r'', 'web.views.home.index', name='home'),
 
     url(r'^mu-25b8a55c-a9fee579-723dcc44-9782bfc2$', 'web.views.blitz.index'),
     url(r'^google3da26e317e8e8cda.html$', 'web.views.google_webmaster.index'),
