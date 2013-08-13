b057d97f279828b51127548330ba2582a69273e3
Nick Terrell <terrelln@umich.edu>
2013-08-13 14:28:28 -0400

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-cd ../
+PROJECT_ROOT="/var/www/knoatom-web/"
+APACHE2_DIR ="/etc/apache2/"
+BACKUP_ROOT="/var/www/knoatom-web-backup/"
+SETTINGS="knoatom.settings.production"
+APPS=( "web" "assignment" "rating" )
 
-# move stuff to web directory
-cp -R * /var/www/knoatom-web/
-
-# remove .git directory from deployment
-rm -rf /var/www/knoatom-web/.git
-
-cd /var/www/knoatom-web
-
-# restore settings file
-cp /var/www/knoatom-backup/settings.py knoatom/
+cd ../ # cd into git project root
+# Pull from github
+git pull origin
+# Copy contents of project root
+cp -rf $PROJECT_ROOT* $BACKUP_ROOT*
+# Delete contents of project root
+rm -rf $PROJECT_ROOT*
+# copy to project root
+cp -r * $PROJECT_ROOT
+# cd into project root
+cd $PROJECT_ROOT
+# Delete .git* files and secret_key.py
+rm -rf .git .gitignore knoatom/settings/secret_key.py
 
 # collect static stuff
-python manage.py collectstatic --noinput
+python manage.py collectstatic --settings=$SETTINGS --noinput
 
-# sync db
+# sync db, we NEED to work on creating south migrations correctly
+#for i in ${APPS[@]}
+#do
+#	python manage.py migrate $i --settings=$SETTINGS --noinput
+#done
 python manage.py syncdb --noinput
 
 # restart apache
