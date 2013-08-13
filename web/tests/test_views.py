a7041ae8f06c1e06d1e2bbe0195f7def9fca86e5
Nick Terrell <terrelln@umich.edu>
2013-08-13 14:29:29 -0400

    Added skeleton TestCases

--- web/tests/test_views.py
+++ web/tests/test_views.py
@@ -1,2 +1,21 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+
+#############################~~~~ web/home.py ~~~~#############################
+class ReportTests(TestCase):
+    pass
+    
+class ClassListTests(TestCase):
+    pass
+    
+class IndexTests(TestCase):
+    pass
+    
+class CategoryTests(TestCase):
+    pass
+    
+class AtomTests(TestCase):
+    pass
+    
+class ClassIndexTests3(TestCase):
+    pass
