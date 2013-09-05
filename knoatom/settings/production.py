6e1032564b8f6d0dacd9cac478699de6e1e9a5f1
terrelln <terrelln@umich.edu>
2013-09-05 12:02:40 -0400

    Changed the static/media roots to match the live servers configuration.

--- knoatom/settings/production.py
+++ knoatom/settings/production.py
@@ -44,10 +44,10 @@ DATABASES = {
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-PROJECT_ROOT = '/var/www/knoatom-web/'
+PROJECT_ROOT = '/var/www/live/'
 # Function to turn relative paths from the project root into absolute paths
 
-MEDIA_ROOT = '/var/www/knoatom-static/media/'
+MEDIA_ROOT = '/var/www/static/media/'
 #MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media/').replace('\\','/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
@@ -59,7 +59,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/knoatom-static/static/'
+STATIC_ROOT = '/var/www/static/static/'
 #STATIC_ROOT = '/var/www/knoatom-static/static/'
 
 # URL prefix for static files.
